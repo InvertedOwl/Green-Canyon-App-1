2d497b5a26732f03605e7a7de994e155367b2e5f
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2023-10-30 06:36:49 -0600

    Notifs

--- src/App.vue
+++ src/App.vue
@@ -54,48 +54,26 @@ import {
   linkSharp,
   notificationsSharp
 } from 'ionicons/icons';
+
+
 // NOTIFICATIONS
 import { PushNotifications } from '@capacitor/push-notifications';
+import { Capacitor } from '@capacitor/core';
+import { FCM } from "@capacitor-community/fcm";
+     
+const isPushNotificationsAvailable = Capacitor.isPluginAvailable('PushNotifications');
 
-const addListeners = async () => {
-  await PushNotifications.addListener('registration', token => {
-    console.info('Registration token: ', token.value);
-  });
+async function notifs() {
+  if (isPushNotificationsAvailable) {
+    await PushNotifications.requestPermissions();
+    await PushNotifications.register();
+  } 
 
-  await PushNotifications.addListener('registrationError', err => {
-    console.error('Registration error: ', err.error);
-  });
-
-  await PushNotifications.addListener('pushNotificationReceived', notification => {
-    console.log('Push notification received: ', notification);
-  });
-
-  await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
-    console.log('Push notification action performed', notification.actionId, notification.inputValue);
-  });
+  FCM.subscribeTo({ topic: "all" })
+  .then((r) => console.log(`subscribed to all`))
+  .catch((err) => console.log(err));
 }
-
-const registerNotifications = async () => {
-  let permStatus = await PushNotifications.checkPermissions();
-
-  if (permStatus.receive === 'prompt') {
-    permStatus = await PushNotifications.requestPermissions();
-  }
-
-  if (permStatus.receive !== 'granted') {
-    throw new Error('User denied permissions!');
-  }
-
-  await PushNotifications.register();
-}
-
-const getDeliveredNotifications = async () => {
-  const notificationList = await PushNotifications.getDeliveredNotifications();
-  console.log('delivered notifications', notificationList);
-}
-registerNotifications();
-addListeners();
-getDeliveredNotifications();
+notifs();
 // END NOTIFICATIONS
 
 const selectedIndex = ref(0);
